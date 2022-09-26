1fb1833023d749ec661a4faccf03dc6485f17bee
Ash Kyd <ash@kyd.com.au>
2022-09-26 23:31:15 +1000

    it's new, it's exciting, it's 2022

---
+++ app.js
@@ -0,0 +1,60 @@
+const getMessages = require("./lib/getMessages");
+const masto = require("masto");
+
+async function post() {
+  console.log(new Date(), "Fetching feed…");
+  const messages = await getMessages({ targetSize: 500 });
+
+  console.log("Logging in…");
+  const bneSocial = await masto.login({
+    url: "https://bne.social",
+    accessToken: process.env.MASTODON_TOKEN,
+  });
+
+  const mastoPosts = messages.map((message) => {
+    console.log("- Posting “" + message + "”");
+    return bneSocial.statuses.create({
+      status: message,
+      visibility: "direct",
+    });
+  });
+
+  await Promise.all(mastoPosts);
+  console.log("Done.");
+}
+
+const cron = require("node-cron");
+
+cron.schedule("59 6 * * *", () => {
+  post();
+});
+
+cron.schedule("35 13 * * *", () => {
+  const bneSocial = await masto.login({
+    url: "https://bne.social",
+    accessToken: process.env.MASTODON_TOKEN,
+  });
+
+  const mastoPosts = messages.map((message) => {
+    console.log("- Posting “" + "haha" + "”");
+    return bneSocial.statuses.create({
+      status: "cronjob fired at 35 13 * * *",
+      visibility: "direct",
+    });
+  });
+});
+
+cron.schedule("35 23 * * *", () => {
+  const bneSocial = await masto.login({
+    url: "https://bne.social",
+    accessToken: process.env.MASTODON_TOKEN,
+  });
+
+  const mastoPosts = messages.map((message) => {
+    console.log("- Posting “" + "hehe" + "”");
+    return bneSocial.statuses.create({
+      status: "cronjob fired at 35 23 * * *",
+      visibility: "direct",
+    });
+  });
+});
