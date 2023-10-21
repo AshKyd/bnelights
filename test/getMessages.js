a6347798196c80ce5c5df7548864be6cf7e252d1
Ash Kyd <ash@kyd.com.au>
2023-10-21 17:25:37 +1000

    strip html entities when using provided copy

--- test/getMessages.js
+++ test/getMessages.js
@@ -11,6 +11,9 @@ const xml2209 = parseXml(
 const xml220929 = parseXml(
   require("fs").readFileSync("./test/assets/2022-09-29.rss", "utf8")
 );
+const htmlEntities = parseXml(
+  require("fs").readFileSync("./test/assets/htmlentities.rss", "utf8")
+);
 
 describe("getMessages", () => {
   before(() => MockDate.set(new Date("2022-09-10T00:00:00.000Z")));
@@ -40,6 +43,18 @@ describe("getMessages", () => {
       const output = getMessages({ items: xml220929, targetSize: 500 });
       assert.deepEqual(output, [
         "Tonight landmarks around the city will be lit white & blue for National Police Remembrance Day",
+      ]);
+    });
+  });
+
+  describe("with html tags", () => {
+    before(() => MockDate.set(new Date("2023-10-21T07:28:31.844Z")));
+    after(() => MockDate.reset());
+    it("should return today's message", () => {
+      const output = getMessages({ items: htmlEntities, targetSize: 500 });
+      assert.deepEqual(output, [
+        "City Hall will be lit Blue and Yellow to celebrate an event being held in City Hall.\n\nThis Light Up is organised by: JCI",
+        "Tonight Victoria Bridge, Reddacliff Place, & Story Bridge will be lit blue for Mental Health Awareness Month - Beyond Blues' Big Blue Table 🎗️",
       ]);
     });
   });
